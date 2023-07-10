2cd5ce952f496a1748a366df23c86ce1ae55443a
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2023-07-10 22:12:36 +0300

    made task2, task3

---
+++ task002/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int GetRandomNumber()
+{
+    int number = new Random().Next(100, 1000); 
+    return number;
+}
+int GetFirstDigit(int number1)
+{
+    return number1 / 100;
+}
+int GetLastDigit(int number1)
+{
+    return number1 % 10;
+}
+void PrintNumber(int num1)
+{
+    Console.Write(num1);
+}
+
+int num = GetRandomNumber();
+PrintNumber(num);
+Console.Write(" -> ");
+int firstDigit = GetFirstDigit(num);
+int lastDigit = GetLastDigit(num);
+PrintNumber(firstDigit);
+PrintNumber(lastDigit);
---
+++ task003/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+int remains = num1%num2;
+if (remains == num2)
+{
+    Console.WriteLine("Ваше числа кратные");
+}
+else
+{
+    Console.WriteLine("Ваши числа некратные, остаток: " + remains);
+}
