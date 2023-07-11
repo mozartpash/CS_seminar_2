65d72d9d5a5bef514c6e7638f61064086cd7140e
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2023-07-11 14:10:11 +0300

    done all tasks

--- task004/Program.cs
+++ task004/Program.cs
@@ -3,7 +3,19 @@
 // 46 -> нет
 // 161 -> да
 
-Console.WriteLine("Введите число");
-int number = int.Parse(Console.ReadLine());
 
 void Multiply(int numb)
+{
+    if (numb % 7 == 0 && numb % 23 == 0)
+    {
+        Console.WriteLine("Данное число кратно и 7 и 23 одновременно");
+    }
+    else
+    {
+        Console.WriteLine("Данное число не кратно и 7 и 23 одновременно");
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Multiply(number);
---
+++ task005/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+void Squere(int number1, int number2)
+{    
+    if (number1 * number1 % number2 == 0 || number2 * number2 % number1 == 0)// || Это означает "или"
+    {
+        Console. WriteLine("-> да");
+    }       
+    else
+    {
+        Console. WriteLine("-> нет");
+    }    
+}
+
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+Squere(num1, num2);
